7577688648aa784f2c36780b8ae2cb49dd46ec6c
Francis Deng <zd2278@columbia.edu>
2025-10-21 09:18:36 -0400

    setup: Imported source code template

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rottenpotatoes::Application.routes.draw do
+  resources :movies
+  # Add new routes here
+
+  resources :movies do
+    get :similar, on: :member
+  end
+
+  root to: redirect('/movies')
+end
