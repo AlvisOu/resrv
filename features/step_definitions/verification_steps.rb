c59295867edfaaf943e9ca99f1e423a06282063f
skysunmingyang <ms6124@columbia.edu>
2025-11-12 20:44:47 -0500

    test: sessions_controller feature tests

--- features/step_definitions/verification_steps.rb
+++ features/step_definitions/verification_steps.rb
@@ -1,3 +1,12 @@
+Given(/^the user "([^"]*)" exists and is unverified with password "([^"]*)"$/) do |email, password|
+  User.find_or_create_by!(email: email) do |user|
+    user.name = email.split('@').first.capitalize
+    user.password = password
+    user.password_confirmation = password
+    user.email_verified_at = nil
+  end
+end
+
 When("I fill in the verification code for {string}") do |email|
   user = User.find_by!(email: email)
   code = user.verification_code
