ce6d28b41fb91d46d4bf1d857be3730780204f5a
Francis Deng <zd2278@columbia.edu>
2025-11-26 16:48:39 -0500

    ui: workspace new and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_24_000000) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_26_213725) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
@@ -118,6 +118,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_24_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "description"
     t.index ["slug"], name: "index_workspaces_on_slug", unique: true
   end
 
